ade59604c8801baf353a7e09afa1df1a504b0267
Samuel <samuellmoraes28@gmaiil.com>
2024-11-01 21:42:47 -0300

    alteração do visual com o ScenneBuild

--- src/main/java/org/usuario/controller/MainController.java
+++ src/main/java/org/usuario/controller/MainController.java
@@ -1,23 +1,28 @@
 package org.usuario.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.usuario.model.Usuario;
 
 public class MainController {
     @FXML
     private TextField txtNome;
 
     @FXML
-    private TextField txtPasword;
+    private TextField txtPassword;
 
     @FXML
     private Button btnSalvar;
 
     @FXML
     public void onBtnSalvarAction() {
-    System.out.println("Clique Salvar");
+    this.Usuario = new Usuario();
+    Usuario.setNome(txtNome.getText());
+    Usuario.setSenha(txtPassword.getText());
+        System.out.println("Nome: " + Usuario.getNome());
+        System.out.println("Senha: " + Usuario.getSenha());
 
     }
 }
